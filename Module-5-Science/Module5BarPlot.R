41fa67ad5899ac1dddee1b72605b41def988e7a1
ianeyk <ian@eykamp.com>
2022-01-19 19:19:31 -0500

    reformat code to be follow the tidy style guide

--- Module-5-Science/Module5BarPlot.R
+++ Module-5-Science/Module5BarPlot.R
@@ -1,37 +1,44 @@
-#Plot of OR and AQI
+# Plot of OR and AQI
 
-#install.packages("ggplot2")
-
-
-#Load libraries for our plot/map
-
-library(ggplot2)
+# install.packages("tidyverse")
 
 
-#load AQI data
-countyAQI <- readr::read_csv("C:/dev/maps/daily_aqi_by_county_2021.csv")
+# Load libraries for our plot/map
 
-#Rename county and state name columns 
-countyAQI$County <- countyAQI$`county Name`
-countyAQI$State <- countyAQI$`State Name`
+library(tidyverse)
+
+
+# load AQI data
+countyAQI <- read_csv("C:/dev/maps/daily_aqi_by_county_2021.csv")
+
+# Rename county and state name columns
+countyAQI <- countyAQI %>% 
+  rename(
+    county = "county Name", 
+    state = "State Name"
+  )
 head(countyAQI)
 
 #select from table only Oregon counties
-OR_AQI <- subset(countyAQI, State == "Oregon")
-
-#group by county and average AQI values for all those that share county name
-AQI1 <- OR_AQI %>% 
-  group_by(County) %>% 
-  summarise(AQIAvg = mean(AQI))
-
-#Check AQI1 (average AQI for Counties)
-head(AQI1)
+OR_AQI <- countyAQI %>% 
+  filter(state == "Oregon") %>% 
+  group_by(county) %>% 
+  summarize(AQIavg = mean(AQI))
+head(OR_AQI)
 
 #lot AQI average against county
-ggplot(AQI1, aes(x = County, y = AQIAvg), fill=County) + #define x and y axis values
-    geom_bar(stat="identity") +
-    coord_flip() + #flips coordinates so labels work better. Delete this line if you wish to have the plot vertical
-    ggtitle("AQI Average for Oregon Counties 2021") + #title of plot
-    xlab("County") + #label x axis
-    ylab("AQI")   #label y axis
-       
+OR_AQI %>% 
+ggplot(mapping = aes(x = county, y = AQIavg), fill = "khaki3") + 
+  geom_bar(stat = "identity") + 
+  ylim(0, 100) + 
+  labs(
+    x = "County", 
+    y = "AQI", 
+    title = "AQI Average for Oregon Counties 2021"
+  ) + 
+  coord_flip() + # flips coordinates so labels work better. 
+                 # Delete this line if you wish to have the plot vertical
+  theme(
+    axis.ticks.y = element_blank(),
+    panel.grid.major.y = element_blank()
+  )
